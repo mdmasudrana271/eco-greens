0560d24c13cb7f9a789cd7329b95552dfd1437a0
mdmasudrana271 <mr7123619@gmail.com>
2025-02-07 00:01:56 +0600

    fix media issue

--- EcoGreens/settings.py
+++ EcoGreens/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
@@ -102,7 +102,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'EcoGreens.wsgi.application'
+WSGI_APPLICATION = 'EcoGreens.wsgi.app'
 
 
 # Database
@@ -174,6 +174,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # additinal added
 # MEDIA_URL = '/media/'
