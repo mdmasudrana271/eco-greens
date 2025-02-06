eed5c5c1b7a4b2799d2c45f001cd4431349e6789
mdmasudrana271 <mr7123619@gmail.com>
2025-02-06 23:14:19 +0600

    fix media issue

--- EcoGreens/settings.py
+++ EcoGreens/settings.py
@@ -177,7 +177,7 @@ STATIC_URL = 'static/'
 
 # additinal added
 # MEDIA_URL = '/media/'
-# MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = BASE_DIR / 'media'
 # MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
